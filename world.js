ebecdba8b42586e104b6cfd041230e2bc2d47fa6
cherrih <cherri.hartigan@gmail.com>
2019-04-01 14:10:34 -0700

    Render main copy

--- world.js
+++ world.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+const el = document.getElementById('world');
+const text = 'Hailing from 1 the land of sheep and kiwis (New Zealand), I am a San Francisco-based 1 Full-Stack developer, Creator and Collaborator. Currently seeking a user-focused Creative Technologist / Front-End Developer role. Fancy a collaboration or interested in chatting? Reach out!';
+const textArr = text.split('');
+
+function drawWorld(){
+  el.innerHTML = '';
+  textArr.forEach(char => {
+    if (char === '1') {
+      el.innerHTML += '<div class="cherry"><img src="images/cherry.png"></div>';
+    } 
+    else if (char === ' ') {
+      el.innerHTML += '<div class="space"></div>';
+    }
+    else {
+      el.innerHTML += `<div class="world-text">${char}</div>`;
+    }
+  })
+}
+drawWorld();
