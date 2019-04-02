d48626251a8eb6f14d58206a3980b94be1ffc11d
cherrih <cherri.hartigan@gmail.com>
2019-04-01 17:00:56 -0700

    Prevent words from spilling onto new lines

--- world.js
+++ world.js
@@ -1,18 +1,20 @@
 const el = document.getElementById('world');
-const text = 'Hailing from 1 the land of sheep and kiwis (New Zealand), I am a San Francisco-based 1 Full-Stack developer, Creator and Collaborator. Currently seeking a user-focused Creative Technologist / Front-End Developer role. Fancy a collaboration or interested in chatting? Reach out!';
-const textArr = text.split('');
+const text = "Hailing from 1 the land of sheep and kiwis (New Zealand), I am a San Francisco-based 1 Full-Stack developer, Creator and Collaborator. Currently seeking a user-focused Creative Technologist / Front-End Developer role. 1 Fancy a collaboration or interested in chatting? Let's talk!";
+const textArr = text.split(' ');
 
 function drawWorld(){
   el.innerHTML = '';
-  textArr.forEach(char => {
-    if (char === '1') {
-      el.innerHTML += '<div class="cherry"><img src="images/cherry.png"></div>';
-    } 
-    else if (char === ' ') {
-      el.innerHTML += '<div class="space"></div>';
-    }
-    else {
-      el.innerHTML += `<div class="world-text">${char}</div>`;
+  textArr.forEach(word => {
+    if (word === '1') {
+      el.innerHTML += '<div class="cherry"><img src="images/cherry.png"></div><div class="space"></div>';
+    } else {
+      let divString = '<div class="world-text">';
+      charArr = word.split('');
+      charArr.forEach(char => {
+        divString += `<span class="world-char">${char}</span>`;
+      })
+      divString += '</div>';
+      el.innerHTML += divString + '<div class="space"></div>';
     }
   })
 }
