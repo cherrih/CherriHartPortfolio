8d7c69220cc069ce74a6e433e340b370aeaf9431
cherrih <cherri.hartigan@gmail.com>
2019-04-01 17:53:00 -0700

    Complete logic for moving pac around the screen

--- world.js
+++ world.js
@@ -1,9 +1,12 @@
 const el = document.getElementById('world');
 const text = "Hailing from 1 the land of sheep and kiwis (New Zealand), I am a San Francisco-based 1 Full-Stack developer, Creator and Collaborator. Currently seeking a user-focused Creative Technologist / Front-End Developer role. 1 Fancy a collaboration or interested in chatting? Let's talk!";
 const textArr = text.split(' ');
+let pac;
+let pacX = 9;
+let pacY = 15.5;
 
 function drawWorld(){
-  el.innerHTML = '<div class="pac"><img src="images/pac.png"></div>';
+  el.innerHTML = '<div id="pac"><img src="images/pac.png"></div>';
   textArr.forEach(word => {
     if (word === '1') {
       el.innerHTML += '<div class="cherry"><img src="images/cherry.png"></div><div class="space"></div>';
@@ -16,6 +19,34 @@ function drawWorld(){
       divString += '</div>';
       el.innerHTML += divString + '<div class="space"></div>';
     }
-  })
+  }) 
+  pac = document.getElementById('pac'); 
+  pac.style.left = `${pacX}vw`;
+  pac.style.top = `${pacY}vh`;
 }
 drawWorld();
+
+
+
+document.onkeydown = function(e) {
+  // move pac left
+  if (e.keyCode === 37) {
+    pacX -= 1;
+    pac.style.left = `${pacX}vw`;
+  }
+  // move pac right
+  if (e.keyCode === 39) {
+    pacX += 1;
+    pac.style.left = `${pacX}vw`;
+  }
+  // move pac up
+  if (e.keyCode === 38) {
+    pacY -= 2;
+    pac.style.top = `${pacY}vh`;
+  }
+  // move pac down
+  if (e.keyCode === 40) {
+    pacY += 2;
+    pac.style.top = `${pacY}vh`;
+  }
+}
