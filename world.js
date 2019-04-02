1195f09e8de6681726a82f3bfb514ff54b60961b
cherrih <cherri.hartigan@gmail.com>
2019-04-02 12:40:10 -0700

    Enable pac to eat letters

--- world.js
+++ world.js
@@ -1,52 +1,130 @@
 const el = document.getElementById('world');
-const text = "Hailing from 1 the land of sheep and kiwis (New Zealand), I am a San Francisco-based 1 Full-Stack developer, Creator and Collaborator. Currently seeking a user-focused Creative Technologist / Front-End Developer role. 1 Fancy a collaboration or interested in chatting? Let's talk!";
-const textArr = text.split(' ');
+const text = 
+  ["2 Hailing from 1 the land of",
+  "sheep and kiwis (New Zealand),", 
+  "I am a San Francisco-based 1", 
+  "Full-Stack developer, Creator and",
+  "Collaborator. 1 Currently seeking a", 
+  "user-focused Creative Technologist /", 
+  "Front-End Developer role. 1", 
+  "Fancy a collaboration or interested", 
+  "in chatting? Let's talk!"];
+
+  
+const textMatrix = [];
+function drawMatrix(){
+  text.forEach(line => {
+    textMatrix.push(line.split(''))
+  });
+}
+drawMatrix();
 let pac;
-let pacX = 9;
-let pacY = 15.5;
+let pacX = 0;
+let pacY = 0;
 
 function drawWorld(){
-  el.innerHTML = '<div id="pac"><img src="images/pac.png"></div>';
-  textArr.forEach(word => {
-    if (word === '1') {
-      el.innerHTML += '<div class="cherry"><img src="images/cherry.png"></div><div class="space"></div>';
-    } else {
-      let divString = '<div class="world-text">';
-      charArr = word.split('');
-      charArr.forEach(char => {
-        divString += `<span class="world-char">${char}</span>`;
-      })
-      divString += '</div>';
-      el.innerHTML += divString + '<div class="space"></div>';
-    }
-  }) 
-  pac = document.getElementById('pac'); 
-  pac.style.left = `${pacX}vw`;
-  pac.style.top = `${pacY}vh`;
+  console.log('DRAWING')
+  el.innerHTML = '<div></div>';
+  textMatrix.forEach(row =>{
+    row.forEach(char => {
+      if (char === '1') {
+        el.innerHTML += '<div class="cherry"><img src="images/cherry.png"></div><div class="space"></div>';
+      } else if (char === '2') {
+        el.innerHTML += '<div class="space"></div><div id="pac"><img src="images/pac.png"></div><div class="space"></div>';
+      } else if (char === ' ' || char === '3') {
+        el.innerHTML += '<div class="space"></div>';
+      } else {
+        el.innerHTML +=`<div class="world-char">${char}</div>`;
+      }        
+      // pac = document.getElementById('pac'); 
+      // pac.style.left = `${pacX}vw`;
+      // pac.style.top = `${pacY}vh`;
+    })
+    el.innerHTML += '<br/>'
+  })
 }
+
+// function drawWorld(){
+//   text.forEach(row => {
+//     const textArr = row.split(' ');
+//     textArr.forEach(word => {
+//       if (word === '1') {
+//         el.innerHTML += '<div class="cherry"><img src="images/cherry.png"></div><div class="space"></div>';
+//       } else if (word === '2') {
+//         el.innerHTML = '<div class="space"></div><div id="pac"><img src="images/pac.png"></div><div class="space"></div>';
+//       } else {
+//         let divString = '<div class="world-text">';
+//         charArr = word.split('');
+//         charArr.forEach(char => {
+//           divString += `<span class="world-char">${char}</span>`;
+//         })
+//         divString += '</div>';
+//         el.innerHTML += divString + '<div class="space"></div>';
+//       }
+//     })
+//     el.innerHTML += '<br/>'
+    
+//     // pac = document.getElementById('pac'); 
+//     // pac.style.left = `${pacX}vw`;
+//     // pac.style.top = `${pacY}vh`;
+//   })
+// }
 drawWorld();
-
-
 
 document.onkeydown = function(e) {
   // move pac left
-  if (e.keyCode === 37) {
+  if (e.keyCode === 37 && pacX > 0) {
+    console.log('left');
+    textMatrix[pacY][pacX] = '3';
     pacX -= 1;
-    pac.style.left = `${pacX}vw`;
+    textMatrix[pacY][pacX] = '2';
+    drawWorld();
   }
   // move pac right
   if (e.keyCode === 39) {
+    console.log('right')
+
+    textMatrix[pacY][pacX] = '3';
     pacX += 1;
-    pac.style.left = `${pacX}vw`;
+    textMatrix[pacY][pacX] = '2';
+    drawWorld();
   }
-  // move pac up
+  // move pac up 
   if (e.keyCode === 38) {
-    pacY -= 2;
-    pac.style.top = `${pacY}vh`;
+    textMatrix[pacY][pacX] = '3';
+    pacY -= 1;
+    textMatrix[pacY][pacX] = '2';
+    drawWorld();
   }
   // move pac down
-  if (e.keyCode === 40) {
-    pacY += 2;
-    pac.style.top = `${pacY}vh`;
+  if (e.keyCode === 40 && pacY < 8) {
+    textMatrix[pacY][pacX] = '3';
+    pacY += 1;
+    textMatrix[pacY][pacX] = '2';
+    drawWorld();
   }
 }
+
+
+// document.onkeydown = function(e) {
+//   // move pac left
+//   if (e.keyCode === 37) {
+//     pacX -= 2;
+//     pac.style.left = `${pacX}vw`;
+//   }
+//   // move pac right
+//   if (e.keyCode === 39) {
+//     pacX += 2;
+//     pac.style.left = `${pacX}vw`;
+//   }
+//   // move pac up
+//   if (e.keyCode === 38) {
+//     pacY -= 2;
+//     pac.style.top = `${pacY}vh`;
+//   }
+//   // move pac down
+//   if (e.keyCode === 40) {
+//     pacY += 2;
+//     pac.style.top = `${pacY}vh`;
+//   }
+// }
