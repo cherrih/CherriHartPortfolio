fd5491410d7813e0e5883a21a4f499dd08362862
cherrih <cherri.hartigan@gmail.com>
2019-04-18 13:33:15 -0700

    Add additional stylings to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,32 +4,32 @@ const Projects = (props) => {
   const projects = [
     {
       title: 'Rogue',
-      description: 'LAUNCHING SOON: Experiential "low fashion" magazine',
-      techStack: 'Javascript, React, HTML, CSS, Matter.js, Webpack'
+      description: 'Launching May 2019: Experiential "low fashion" magazine',
+      techStack: 'Javascript, HTML, CSS, React, Webpack, Matter.js, Three.js, WebGL, Git'
     },
     {
       title: 'Queertrip',
       description: 'Full-stack travel app for the LGBTQIA+ community',
-      techStack: 'Javascript, React, HTML, CSS, MongoDB, Express, Node.js, Webpack, Google Maps API',
+      techStack: 'Javascript, React, HTML, CSS, MongoDB, Mongoose.js, Express, Node.js, Webpack, Google Maps API, Git',
     },
     {
       title: 'Watch.io',
       description: 'Full-stack retail product app for watch company',
-      techStack: 'Javascript, React, HTML, CSS, MySQL, Express, AWS, Node.js, Webpack, Jest/Enzyme'
+      techStack: 'Javascript, React, HTML, CSS, MySQL, Express, AWS, Node.js, Webpack, Jest/Enzyme, Git'
     },
     {
       title: 'FindTables',
       description: 'System design of an online restaurant reservation service',
-      techStack: 'Javascript, MongoDB, PostgreSQL, Express, AWS, Node.js, Webpack, NGINX, K6, New Relic, loader.io',
+      techStack: 'Javascript, MongoDB, Mongoose.js, PostgreSQL, Express, AWS, Node.js, Webpack, NGINX, K6, New Relic, loader.io, Git',
     },
     {
       title: 'Belen Tenorio Portfolio',
-      description: 'Portfolio development for product/brand designer Belen Tenorio',
+      description: 'Portfolio development for product/brand designer',
       techStack: 'Vanilla Javascript, HTML, CSS, Sketch',
     },
     {
       title: 'Anna Degenaar Portfolio',
-      description: 'Portfolio design and development for photographer/writer Anna Degenaar',
+      description: 'Portfolio design and development for photographer/writer',
       techStack: 'Javascript, jQuery, HTML, CSS, Sketch',
     }
   ]
