d3a3b1ca9b570d024ecb1d492613bc93d004b7bc
cherrih <cherri.hartigan@gmail.com>
2019-04-21 12:58:28 -0700

    Refactor variable names

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,24 @@ class Home extends React.Component {
     super(props);
     this.state = {
       matrix: [],
-      pacX: 6,
-      pacY: 1,
-      left: false,
+      pac: {
+        x: 6,
+        y: 1,
+        left: false,
+      },
       score: 0,
+      g7: {
+        x: 42,
+        y: 7
+      },
+      g8: {
+        x: 31,
+        y: 10
+      },
+      g9: {
+        x: 43,
+        y: 1
+      }
     }
     this.handleKeyDown = this.handleKeyDown.bind(this);
   }
@@ -27,12 +41,12 @@ class Home extends React.Component {
       "333333in chatting? Let's talk!33333333333333",
       "33333333333333333333333333333338333333333333"];
     
-    const textMatrix = [];
+    const matrix = [];
     text.forEach(line => {
-      textMatrix.push(line.split(''))
+      matrix.push(line.split(''))
     });
     this.setState({
-      matrix: textMatrix
+      matrix: matrix
     })
   }
   componentWillMount() {
@@ -45,58 +59,58 @@ class Home extends React.Component {
 
   handleKeyDown(e) {
     e.preventDefault();
-    let textMatrix = this.state.matrix;
-    let x = this.state.pacX;
-    let y = this.state.pacY;
     let pieceToEat;
+    let { g7, g8, g9, matrix, pac, score } = this.state;
     // move pac left
     if (e.keyCode === 37 && x > 0) {
-      this.state.left = true;
-      textMatrix[y][x] = '3';
-      this.state.pacX -= 1;
-      pieceToEat = textMatrix[y][x - 1];
-      textMatrix[y][x - 1] = '2';
+      pac.left = true;
+      matrix[y][x] = '3';
+      pac.x -= 1;
+      pieceToEat = matrix[y][x - 1];
+      matrix[y][x - 1] = '2';
     }
     // move pac right
-    if (e.keyCode === 39 && x < textMatrix[y].length - 1) {
-      this.state.left = false;
-      textMatrix[y][x] = '3';
-      this.state.pacX += 1;
-      pieceToEat = textMatrix[y][x + 1];
-      textMatrix[y][x + 1] = '2';
+    if (e.keyCode === 39 && x < matrix[y].length - 1) {
+      pac.left = false;
+      matrix[y][x] = '3';
+      pac.x += 1;
+      pieceToEat = matrix[y][x + 1];
+      matrix[y][x + 1] = '2';
     }
     // move pac up 
     if (e.keyCode === 38 && y > 0) {
-      this.state.left = false;
-      textMatrix[y][x] = '3';
-      this.state.pacY -= 1;
-      pieceToEat = textMatrix[y - 1][x];
-      textMatrix[y - 1][x] = '2';
+      pac.left = false;
+      matrix[y][x] = '3';
+      pac.y -= 1;
+      pieceToEat = matrix[y - 1][x];
+      matrix[y - 1][x] = '2';
     }
     // move pac down
     if (e.keyCode === 40 && y < 10) {
-      this.state.left = false;
-      textMatrix[y][x] = '3';
-      this.state.pacY += 1;
-      pieceToEat = textMatrix[y + 1][x];
-      textMatrix[y + 1][x] = '2';
+      pac.left = false;
+      matrix[y][x] = '3';
+      pac.y += 1;
+      pieceToEat = matrix[y + 1][x];
+      matrix[y + 1][x] = '2';
     }
     if (pieceToEat === '1') {
-      this.state.score += 10;
+      score += 10;
     } else if (pieceToEat === '7' || pieceToEat === '8' || pieceToEat === '9'){
-      this.state.score += 20;
+      score += 20;
     } else if (pieceToEat && pieceToEat !== '3') {
-      this.state.score += 2;
+      score += 2;
     } 
+    
     this.setState({
-      matrix: textMatrix
+      matrix: matrix
     })
   }
   render() {
+    const { matrix, pac, score } = this.state;
     return (
       <div>
         <div className="world-container">
-          <World matrix={this.state.matrix} left={this.state.left}/>
+          <World matrix={matrix} left={pac.left}/>
         </div>
         <div className="home-footer">
           <div className="lives">
@@ -104,7 +118,7 @@ class Home extends React.Component {
             <img src="images/life.png"/>
           </div>
           <div className="score">
-            <div id="points">{this.state.score}</div>
+            <div id="points">{score}</div>
             <div>points</div>
           </div>
           <div className="projects" onClick={this.props.toggleProjects}>Projects <img src="images/arrow.png"/></div>
