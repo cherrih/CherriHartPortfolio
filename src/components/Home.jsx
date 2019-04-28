7421e9622eb1eb6ca00e19906297b7c5ceec3228
cherrih <cherri.hartigan@gmail.com>
2019-04-27 21:51:44 -0700

    Remove repeated code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,25 +10,29 @@ class Home extends React.Component {
         x: 6,
         y: 1,
         left: false,
+        alive: true
       },
       score: 0,
       g7: {
         x: 42,
         y: 7,
         n: '7',
-        dir: 'Up'
+        dir: 'Up',
+        alive: true
       },
       g8: {
         x: 31,
         y: 10,
         n: '8',
-        dir: 'Left'
+        dir: 'Left',
+        alive: true
       },
       g9: {
         x: 43,
         y: 1,
         n: '9',
-        dir: 'Left'
+        dir: 'Left',
+        alive: true
       }
     }
     this.handleKeyDown = this.handleKeyDown.bind(this);
@@ -84,7 +88,7 @@ class Home extends React.Component {
   }
   moveGhost(g) {
     const dir = `move${g.dir}`;
-    this[dir](g);
+    if (g.alive) this[dir](g);
     this.forceUpdate();
   }
   componentDidMount() {
@@ -128,131 +132,61 @@ class Home extends React.Component {
     e.preventDefault();
     let pieceToEat;
     let score = 0;
-    let { g7, g8, g9, matrix, pac } = this.state;
-    if (e.keyCode === 37) {
-      // move pac left
-      if (pac.x > 0) {
-        pac.left = true;
-        matrix[pac.y][pac.x] = '3';
-        pac.x -= 1;
-        pieceToEat = matrix[pac.y][pac.x];
-        matrix[pac.y][pac.x] = '2';
+    let { matrix, pac } = this.state;
+    if (pac.alive) {
+      if (e.keyCode === 37) {
+        // move pac left
+        if (pac.x > 0) {
+          pac.left = true;
+          matrix[pac.y][pac.x] = '3';
+          pac.x -= 1;
+          pieceToEat = matrix[pac.y][pac.x];
+          matrix[pac.y][pac.x] = '2';
+        }
       }
-      // move g9 right
-      if (g9.x < matrix[pac.y].length - 1 && matrix[g9.y][g9.x + 1] === '3') {
-        matrix[g9.y][g9.x] = '3';
-        g9.x += 1;
-        matrix[g9.y][g9.x] = '9';
+      // move pac right
+      if (e.keyCode === 39) {
+        if (pac.x < matrix[pac.y].length - 1){
+          pac.left = false;
+          matrix[pac.y][pac.x] = '3';
+          pac.x += 1;
+          pieceToEat = matrix[pac.y][pac.x];
+          matrix[pac.y][pac.x] = '2';
+        }
       }
-      // move g8 right
-      if (g8.x < matrix[pac.y].length - 1 && matrix[g8.y][g8.x + 1] === '3') {
-        matrix[g8.y][g8.x] = '3';
-        g8.x += 1;
-        matrix[g8.y][g8.x] = '8';
+      if (e.keyCode === 38) {
+        // move pac up 
+        if (pac.y > 0) {
+          pac.left = false;
+          matrix[pac.y][pac.x] = '3';
+          pac.y -= 1;
+          pieceToEat = matrix[pac.y][pac.x];
+          matrix[pac.y][pac.x] = '2';
+        }
       }
-      // move g7 down
-      if (g7.y < matrix.length - 1 && matrix[g7.y + 1][g7.x] === '3') {
-        matrix[g7.y][g7.x] = '3';
-        g7.y += 1;
-        matrix[g7.y][g7.x] = '7';
+      if (e.keyCode === 40 ) {
+        // move pac down
+        if (pac.y < matrix.length - 1) {
+          pac.left = false;
+          matrix[pac.y][pac.x] = '3';
+          pac.y += 1;
+          pieceToEat = matrix[pac.y][pac.x];
+          matrix[pac.y][pac.x] = '2';
+        }
       }
+      if (pieceToEat === '1') {
+        score += 10;
+      } else if (pieceToEat === '7' || pieceToEat === '8' || pieceToEat === '9'){
+        score += 20;
+      } else if (pieceToEat && pieceToEat !== '3') {
+        score += 2;
+      } 
+      
+      this.setState({
+        score: this.state.score += score,
+        matrix: matrix
+      })
     }
-    // move pac right
-    if (e.keyCode === 39) {
-      if (pac.x < matrix[pac.y].length - 1){
-        pac.left = false;
-        matrix[pac.y][pac.x] = '3';
-        pac.x += 1;
-        pieceToEat = matrix[pac.y][pac.x];
-        matrix[pac.y][pac.x] = '2';
-      }
-      // move g9 left
-      if (g9.x > 0 && matrix[g9.y][g9.x - 1] === '3') {
-        matrix[g9.y][g9.x] = '3';
-        g9.x -= 1;
-        matrix[g9.y][g9.x] = '9';
-      }
-      // move g8 left
-      if (g8.x > 0 && matrix[g8.y][g8.x - 1] === '3') {
-        matrix[g8.y][g8.x] = '3';
-        g8.x -= 1;
-        matrix[g8.y][g8.x] = '8';
-      }
-      // move g7 up
-      if (g7.y > 0 && matrix[g7.y - 1][g7.x] === '3') {
-        matrix[g7.y][g7.x] = '3';
-        g7.y -= 1;
-        matrix[g7.y][g7.x] = '7';
-      }
-    }
-    if (e.keyCode === 38) {
-      // move pac up 
-      if (pac.y > 0) {
-        pac.left = false;
-        matrix[pac.y][pac.x] = '3';
-        pac.y -= 1;
-        pieceToEat = matrix[pac.y][pac.x];
-        matrix[pac.y][pac.x] = '2';
-      }
-      // move g9 up
-      if (g9.y > 0 && matrix[g9.y - 1][g9.x] === '3') {
-        matrix[g9.y][g9.x] = '3';
-        g9.y -= 1;
-        matrix[g9.y][g9.x] = '9';
-      }
-      // move g8 down
-      if (g8.y < matrix.length - 1 && matrix[g8.y + 1][g8.x] === '3') {
-        matrix[g8.y][g8.x] = '3';
-        g8.y += 1;
-        matrix[g8.y][g8.x] = '8';
-      }
-      // move g7 right
-      if (g7.x < matrix[pac.y].length - 1 && matrix[g7.y][g7.x + 1] === '3') {
-        matrix[g7.y][g7.x] = '3';
-        g7.x += 1;
-        matrix[g7.y][g7.x] = '7';
-      }
-    }
-    if (e.keyCode === 40 ) {
-      // move pac down
-      if (pac.y < matrix.length - 1) {
-        pac.left = false;
-        matrix[pac.y][pac.x] = '3';
-        pac.y += 1;
-        pieceToEat = matrix[pac.y][pac.x];
-        matrix[pac.y][pac.x] = '2';
-      }
-      // move g9 down
-      if (g9.y < matrix.length - 1 && matrix[g9.y + 1][g9.x] === '3') {
-        matrix[g9.y][g9.x] = '3';
-        g9.y += 1;
-        matrix[g9.y][g9.x] = '9';
-      }
-      // move g8 up
-      if (g8.y > 0 && matrix[g8.y - 1][g8.x] === '3') {
-        matrix[g8.y][g8.x] = '3';
-        g8.y -= 1;
-        matrix[g8.y][g8.x] = '8';
-      }
-      // move g7 left
-      if (g7.x > 0 && matrix[g7.y][g7.x - 1] === '3') {
-        matrix[g7.y][g7.x] = '3';
-        g7.x -= 1;
-        matrix[g7.y][g7.x] = '7';
-      }
-    }
-    if (pieceToEat === '1') {
-      score += 10;
-    } else if (pieceToEat === '7' || pieceToEat === '8' || pieceToEat === '9'){
-      score += 20;
-    } else if (pieceToEat && pieceToEat !== '3') {
-      score += 2;
-    } 
-    
-    this.setState({
-      score: this.state.score += score,
-      matrix: matrix
-    })
   }
   render() {
     const { matrix, pac, score } = this.state;
