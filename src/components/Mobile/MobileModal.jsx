ca9a5f08a1bdec4798e1fcfb2afc7a5e3c8c6ab2
cherrih <cherri.hartigan@gmail.com>
2019-05-05 22:44:05 -0700

    Remove extraneous return statement

--- src/components/Mobile/MobileModal.jsx
+++ src/components/Mobile/MobileModal.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 
-const MobileModal = ({ close, isModal }) => {
-  return (
-    <div className="mobile-modal-wrapper" onClick={close}>
-      <div className="mobile-modal">
-        <div className="close-modal-btn" onClick={close}><img src="https://s3-us-west-1.amazonaws.com/cherri-portfolio/xBlack.png"/></div>
-        <div className="mobile-modal-text">
-          <div className="mobile-modal-title">
-            Welcome!
-          </div>
-          <div className="mobile-modal-body">
-            <p>
-              To play the exclusive Pac game and for a chance to win a special price, please view this site on a bigger screen. Otherwise, scroll down to peruse my projects.
-            </p>
-          </div>
+const MobileModal = ({ close }) => (
+  <div className="mobile-modal-wrapper" onClick={close}>
+    <div className="mobile-modal">
+      <div className="close-modal-btn" onClick={close}><img src="https://s3-us-west-1.amazonaws.com/cherri-portfolio/xBlack.png"/></div>
+      <div className="mobile-modal-text">
+        <div className="mobile-modal-title">
+          Welcome!
+        </div>
+        <div className="mobile-modal-body">
+          <p>
+            To play the exclusive Pac game and for a chance to win a special price, please view this site on a bigger screen. Otherwise, scroll down to peruse my projects.
+          </p>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default MobileModal;
